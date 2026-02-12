60b15343a7fa871a490a25c1da7af6e322f7bbb5
Zenodesign20 <boythitipong2539@gmail.com>
2026-02-13 03:06:10 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 # ================= CONFIG =================
 TOKEN = os.getenv("DISCORD_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))  # ตั้งใน Environment แล้ว
+ADMIN_ID = 1392851942480412822
 DATA_FILE = "members.json"
 
 DURATION_DAYS = 30
